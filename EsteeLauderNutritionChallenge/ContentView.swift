570d5dee9f42e29ff86f5146e3dad0f0f4334f65
Rithika Manchala <manchalarithika@gmail.com>
2023-06-19 22:42:03 -0500

    Initial Commit

---
+++ EsteeLauderNutritionChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EsteeLauderNutritionChallenge
+//
+//  Created by Ravi Manchala on 6/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EsteeLauderNutritionChallenge/EsteeLauderNutritionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderNutritionChallengeApp.swift
+//  EsteeLauderNutritionChallenge
+//
+//  Created by Ravi Manchala on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderNutritionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
